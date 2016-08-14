4818a6a1fa7227d0c40aec1ef6f0636911bc241d
Tom <support@generatepress.com>
2016-08-13 23:43:23 -0700

    Wrap get_permalink() in esc_url()

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -5,45 +5,49 @@
  * @package GeneratePress
  */
 if ( ! function_exists( 'generate_paging_nav' ) ) :
-	function generate_paging_nav() {
-		// Don't print empty markup if there's only one page.
-		if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
-			return;
-		}
-
-		$paged        = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
-		$pagenum_link = html_entity_decode( get_pagenum_link() );
-		$query_args   = array();
-		$url_parts    = explode( '?', $pagenum_link );
-
-		if ( isset( $url_parts[1] ) ) {
-			wp_parse_str( $url_parts[1], $query_args );
-		}
-
-		$pagenum_link = remove_query_arg( array_keys( $query_args ), $pagenum_link );
-		$pagenum_link = trailingslashit( $pagenum_link ) . '%_%';
-
-		$format  = $GLOBALS['wp_rewrite']->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
-		$format .= $GLOBALS['wp_rewrite']->using_permalinks() ? user_trailingslashit( 'page/%#%', 'paged' ) : '?paged=%#%';
-
-		// Set up paginated links.
-		$links = paginate_links( array(
-			'base'     => $pagenum_link,
-			'format'   => $format,
-			'total'    => $GLOBALS['wp_query']->max_num_pages,
-			'current'  => $paged,
-			'mid_size' => apply_filters( 'generate_pagination_mid_size', 1 ),
-			'add_args' => array_map( 'urlencode', $query_args ),
-			'prev_text' => __( '&larr; Previous', 'generatepress' ),
-			'next_text' => __( 'Next &rarr;', 'generatepress' ),
-		) );
-
-		if ( $links ) :
-
-			echo $links; 
-
-		endif;
+/**
+ * Build the pagination links
+ * @since 1.3.35
+ */
+function generate_paging_nav() {
+	// Don't print empty markup if there's only one page.
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
 	}
+
+	$paged        = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
+	$pagenum_link = html_entity_decode( get_pagenum_link() );
+	$query_args   = array();
+	$url_parts    = explode( '?', $pagenum_link );
+
+	if ( isset( $url_parts[1] ) ) {
+		wp_parse_str( $url_parts[1], $query_args );
+	}
+
+	$pagenum_link = remove_query_arg( array_keys( $query_args ), $pagenum_link );
+	$pagenum_link = trailingslashit( $pagenum_link ) . '%_%';
+
+	$format  = $GLOBALS['wp_rewrite']->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
+	$format .= $GLOBALS['wp_rewrite']->using_permalinks() ? user_trailingslashit( 'page/%#%', 'paged' ) : '?paged=%#%';
+
+	// Set up paginated links.
+	$links = paginate_links( array(
+		'base'     => $pagenum_link,
+		'format'   => $format,
+		'total'    => $GLOBALS['wp_query']->max_num_pages,
+		'current'  => $paged,
+		'mid_size' => apply_filters( 'generate_pagination_mid_size', 1 ),
+		'add_args' => array_map( 'urlencode', $query_args ),
+		'prev_text' => __( '&larr; Previous', 'generatepress' ),
+		'next_text' => __( 'Next &rarr;', 'generatepress' ),
+	) );
+
+	if ( $links ) :
+
+		echo $links; 
+
+	endif;
+}
 endif;
 
 if ( ! function_exists( 'generate_content_nav' ) ) :
@@ -248,7 +252,7 @@ if ( ! function_exists( 'generate_excerpt_more' ) ) :
  */
 add_filter( 'excerpt_more', 'generate_excerpt_more' );
 function generate_excerpt_more( $more ) {
-	return ' ... <a title="' . esc_attr( get_the_title() ) . '" class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read more', 'generatepress') . '</a>';
+	return ' ... <a title="' . esc_attr( get_the_title() ) . '" class="read-more" href="'. esc_url( get_permalink( get_the_ID() ) ) . '">' . __('Read more', 'generatepress') . '</a>';
 }
 endif;
 
