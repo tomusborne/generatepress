2e1c50aa9203d532ca85609bf25f62deb8e0ba72
Tom <support@generatepress.com>
2016-08-14 14:07:32 -0700

    Clean up sidebar files

--- sidebar-left.php
+++ sidebar-left.php
@@ -9,11 +9,8 @@ $generate_settings = wp_parse_args(
 	generate_get_defaults() 
 );
 
-$navigation_active = false;
-
 // If the navigation is set in the sidebar, set variable to true
-if ( 'nav-left-sidebar' == $generate_settings['nav_position_setting'] )
-	$navigation_active = true;
+$navigation_active = ( 'nav-left-sidebar' == $generate_settings['nav_position_setting'] ) ? true : false;
 
 // If the secondary navigation is set in the sidebar, set variable to true
 if ( function_exists( 'generate_secondary_nav_get_defaults' ) ) :
@@ -25,25 +22,25 @@ if ( function_exists( 'generate_secondary_nav_get_defaults' ) ) :
 		$navigation_active = true;
 endif;
 ?>
-	<div id="left-sidebar" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary" <?php generate_left_sidebar_class(); ?>>
-		<div class="inside-left-sidebar">
-			<?php do_action( 'generate_before_left_sidebar_content' ); ?>
-			<?php if ( ! dynamic_sidebar( 'sidebar-2' ) ) : ?>
+<div id="left-sidebar" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary" <?php generate_left_sidebar_class(); ?>>
+	<div class="inside-left-sidebar">
+		<?php do_action( 'generate_before_left_sidebar_content' ); ?>
+		<?php if ( ! dynamic_sidebar( 'sidebar-2' ) ) : ?>
 
-				<?php if ( false == $navigation_active ) : ?>
-					<aside id="search" class="widget widget_search">
-						<?php get_search_form(); ?>
-					</aside>
+			<?php if ( false == $navigation_active ) : ?>
+				<aside id="search" class="widget widget_search">
+					<?php get_search_form(); ?>
+				</aside>
 
-					<aside id="archives" class="widget">
-						<h4 class="widget-title"><?php _e( 'Archives', 'generatepress' ); ?></h4>
-						<ul>
-							<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-						</ul>
-					</aside>
-				<?php endif; ?>
+				<aside id="archives" class="widget">
+					<h4 class="widget-title"><?php _e( 'Archives', 'generatepress' ); ?></h4>
+					<ul>
+						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+					</ul>
+				</aside>
+			<?php endif; ?>
 
-			<?php endif; // end sidebar widget area ?>
-			<?php do_action( 'generate_after_left_sidebar_content' ); ?>
-		</div><!-- .inside-left-sidebar -->
-	</div><!-- #secondary -->
+		<?php endif; // end sidebar widget area ?>
+		<?php do_action( 'generate_after_left_sidebar_content' ); ?>
+	</div><!-- .inside-left-sidebar -->
+</div><!-- #secondary -->
--- sidebar.php
+++ sidebar.php
@@ -9,11 +9,8 @@ $generate_settings = wp_parse_args(
 	generate_get_defaults() 
 );
 
-$navigation_active = false;
-
 // If the navigation is set in the sidebar, set variable to true
-if ( 'nav-right-sidebar' == $generate_settings['nav_position_setting'] )
-	$navigation_active = true;
+$navigation_active = ( 'nav-right-sidebar' == $generate_settings['nav_position_setting'] ) ? true : false;
 
 // If the secondary navigation is set in the sidebar, set variable to true
 if ( function_exists( 'generate_secondary_nav_get_defaults' ) ) :
@@ -25,23 +22,23 @@ if ( function_exists( 'generate_secondary_nav_get_defaults' ) ) :
 		$navigation_active = true;
 endif;
 ?>
-	<div id="right-sidebar" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary" <?php generate_right_sidebar_class(); ?>>
-		<div class="inside-right-sidebar">
-			<?php do_action( 'generate_before_right_sidebar_content' ); ?>
-			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-				<?php if ( false == $navigation_active ) : ?>
-					<aside id="search" class="widget widget_search">
-						<?php get_search_form(); ?>
-					</aside>
+<div id="right-sidebar" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary" <?php generate_right_sidebar_class(); ?>>
+	<div class="inside-right-sidebar">
+		<?php do_action( 'generate_before_right_sidebar_content' ); ?>
+		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+			<?php if ( false == $navigation_active ) : ?>
+				<aside id="search" class="widget widget_search">
+					<?php get_search_form(); ?>
+				</aside>
 
-					<aside id="archives" class="widget">
-						<h4 class="widget-title"><?php _e( 'Archives', 'generatepress' ); ?></h4>
-						<ul>
-							<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-						</ul>
-					</aside>
-				<?php endif; ?>
-			<?php endif; // end sidebar widget area ?>
-			<?php do_action( 'generate_after_right_sidebar_content' ); ?>
-		</div><!-- .inside-right-sidebar -->
-	</div><!-- #secondary -->
+				<aside id="archives" class="widget">
+					<h4 class="widget-title"><?php _e( 'Archives', 'generatepress' ); ?></h4>
+					<ul>
+						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+					</ul>
+				</aside>
+			<?php endif; ?>
+		<?php endif; // end sidebar widget area ?>
+		<?php do_action( 'generate_after_right_sidebar_content' ); ?>
+	</div><!-- .inside-right-sidebar -->
+</div><!-- #secondary -->
