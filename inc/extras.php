2733825e79051c387560504416a13fd70d10199c
Tom <support@generatepress.com>
2016-08-13 23:42:07 -0700

    Move add_filter call

--- inc/extras.php
+++ inc/extras.php
@@ -11,11 +11,11 @@ if ( ! function_exists( 'generate_page_menu_args' ) ) :
 /**
  * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
  */
+add_filter( 'wp_page_menu_args', 'generate_page_menu_args' );
 function generate_page_menu_args( $args ) {
 	$args['show_home'] = true;
 	return $args;
 }
-add_filter( 'wp_page_menu_args', 'generate_page_menu_args' );
 endif;
 
 if ( ! function_exists( 'generate_body_classes' ) ) :
