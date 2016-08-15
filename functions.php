d5eea510d9e5bd83419e7d579b1772c6645f237b
Tom <support@generatepress.com>
2016-08-14 20:07:35 -0700

    Bump 1.3.36

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package GeneratePress
  */
 	
-define( 'GENERATE_VERSION', '1.3.34');
+define( 'GENERATE_VERSION', '1.3.36');
 define( 'GENERATE_URI', get_template_directory_uri() );
 define( 'GENERATE_DIR', get_template_directory() );
 
