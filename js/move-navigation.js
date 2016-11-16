732f75ee3992cbc932aa962b1a115e58fa6161c2
tomusborne <tom@edge22.com>
2016-11-16 12:01:52 -0800

    Fix navigation search bug on mobile

--- js/move-navigation.js
+++ js/move-navigation.js
@@ -1,7 +1,8 @@
 jQuery(window).load(function($) {
-	var mobile, widthTimer;
+	var mobile, widthTimer, width;
 	mobile = jQuery( '.menu-toggle' );
-			
+	width = jQuery( window ).width();
+
 	function generateCheckWidth() {
 		if ( ! jQuery( '.gen-sidebar-nav' ) )
 			return;
@@ -12,13 +13,24 @@ jQuery(window).load(function($) {
 			jQuery('#site-navigation:first').appendTo('.gen-sidebar-nav');
 		}
 	}
-			
+
 	if ( mobile.is( ':visible' ) ) {
 		generateCheckWidth();
 	}
-			
-	jQuery(window).resize(function() {
-		clearTimeout(widthTimer);
-		widthTimer = setTimeout(generateCheckWidth, 100);
+
+	jQuery( window ).resize(function() {
+		if ( jQuery(window).width() != width ) {
+			clearTimeout(widthTimer);
+			widthTimer = setTimeout(generateCheckWidth, 100);
+			width = jQuery(window).width();
+		}
+	});
+	
+	jQuery( window ).on( "orientationchange", function( event ) {
+		if ( jQuery(window).width() != width ) {
+			clearTimeout(widthTimer);
+			widthTimer = setTimeout(generateCheckWidth, 100);
+			width = jQuery(window).width();
+		}
 	});
 });
