dfce9905ae6c03ffc7b58c160728f53f64d622f1
tomusborne <tom@edge22.com>
2016-11-21 14:12:22 -0800

    Remove group-blog class from the <body> element
    is_multi_author() is a somewhat expensive function, and I doubt many
    people use this class.

--- inc/extras.php
+++ inc/extras.php
@@ -96,11 +96,6 @@ function generate_body_classes( $classes )
 	} else {
 		$classes[] = 'dropdown-hover';
 	}
-	
-	// Adds a class of group-blog to blogs with more than 1 published author
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
 
 	return $classes;
 }
