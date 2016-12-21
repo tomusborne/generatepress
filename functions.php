af83285b9ba9602d88894cf60f408e91e9c50972
tomusborne <tom@edge22.com>
2016-12-21 11:59:32 -0800

    Merge copyright area into one function

--- functions.php
+++ functions.php
@@ -453,17 +453,7 @@ add_action('generate_credits','generate_add_footer_info');
 function generate_add_footer_info()
 {
 	?>
-	<span class="copyright"><?php _e('Copyright','generatepress');?> &copy; <?php echo date('Y'); ?></span> <?php do_action('generate_copyright_line');?>
-	<?php
-}
-endif;
-
-if ( ! function_exists( 'generate_add_login_attribution' ) ) :
-add_action('generate_copyright_line','generate_add_login_attribution');
-function generate_add_login_attribution()
-{
-	?>
-	&#x000B7; <a href="<?php echo esc_url('https://generatepress.com');?>" target="_blank" title="GeneratePress" itemprop="url">GeneratePress</a>
+	<span class="copyright"><?php _e('Copyright','generatepress');?> &copy; <?php echo date('Y'); ?></span> &#x000B7; <a href="<?php echo esc_url('https://generatepress.com');?>" target="_blank" title="GeneratePress" itemprop="url">GeneratePress</a>
 	<?php
 }
 endif;
