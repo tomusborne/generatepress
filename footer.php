f92e91f585d8002acd65695f4aacc5fcc5272692
tomusborne <tom@edge22.com>
2017-01-19 13:39:37 -0800

    Add new generate_footer hook

--- footer.php
+++ footer.php
@@ -16,10 +16,10 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 <?php do_action('generate_before_footer'); ?>
 <div <?php generate_footer_class(); ?>>
 	<?php 
-	do_action('generate_before_footer_content');
-	
+	do_action( 'generate_before_footer_content' );
+	do_action( 'generate_footer' );
+	do_action( 'generate_after_footer_content' ); 
 	?>
-	<?php do_action( 'generate_after_footer_content' ); ?>
 </div><!-- .site-footer -->
 
 <?php wp_footer(); ?>
