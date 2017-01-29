965dc9bf3db97367a2e6467191fda5cdd3728ccc
Luiz Paulo "Bills <luizpbills@gmail.com>
2017-01-29 15:50:07 -0200

    Added `generate_mobile_breakpoint_media` filter
    And reverted `generate_mobile_breakpoint` filter to maintain compatibility.

--- inc/add-ons/spacing.php
+++ inc/add-ons/spacing.php
@@ -264,7 +264,8 @@ function generate_spacing_css()
 	}
 	
 	// Add mobile padding to the content
-	$mobile = apply_filters( 'generate_mobile_breakpoint', '(max-width:768px)' );
+	$mobile = apply_filters( 'generate_mobile_breakpoint', '768px' ); // to maintain compatibility
+	$mobile = apply_filters( 'generate_mobile_breakpoint_media', "(max-width: ${mobile})" );
 	$mobile_content_padding = ( isset( $spacing_settings[ 'mobile_content_padding' ] ) ) ? absint( $spacing_settings[ 'mobile_content_padding' ] ) : '30';
 	$output .= '@media ' . esc_attr( $mobile ) . ' {.separate-containers .inside-article, .separate-containers .comments-area, .separate-containers .page-header, .separate-containers .paging-navigation, .one-container .site-content {padding: ' . $mobile_content_padding . 'px;}}';
 	
