34a4c4a576a215f6918b8a4ec624d6fee4f2cdd5
tomusborne <tom@edge22.com>
2017-04-05 10:42:24 -0700

    Add new WooCommerce 3.0 features

--- inc/plugin-compat.php
+++ inc/plugin-compat.php
@@ -2,14 +2,25 @@
 // No direct access, please
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+if ( ! function_exists( 'generate_woocommerce_setup' ) ) :
 /** 
- * Remove default WooCommerce wrappers
- * @since 1.3.22
+ * Set up WooCommerce
+ * @since 1.3.47
  */
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
-add_action('woocommerce_sidebar','generate_construct_sidebars');
+add_action( 'after_setup_theme','generate_woocommerce_setup' );
+function generate_woocommerce_setup() {
+	// Add support for WC features
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
+	
+	//Remove default WooCommerce wrappers
+	remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+	remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+	remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
+	add_action('woocommerce_sidebar','generate_construct_sidebars');
+}
+endif;
 
 if ( ! function_exists( 'generate_woocommerce_start' ) ) :
 /** 
