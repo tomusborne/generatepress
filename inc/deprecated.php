6bc6d8b14390103f13dd130cc080b098fb5c9223
tomusborne <tom@edge22.com>
2017-05-26 01:40:53 -0700

    Remove image caption padding

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -97,3 +97,16 @@ function generate_no_addons()
 		endif;
 }
 endif;
+
+if ( ! function_exists( 'generate_remove_caption_padding' ) ) :
+/**
+ * Remove WordPress's default padding on images with captions
+ *
+ * @param int $width Default WP .wp-caption width (image width + 10px)
+ * @return int Updated width to remove 10px padding
+ * @deprecated 1.4
+ */
+function generate_remove_caption_padding( $width ) {
+	return $width - 10;
+}
+endif;
