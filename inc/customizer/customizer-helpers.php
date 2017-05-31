6d0cfce930de6bd96c9a285c81f303e9abe087fe
tomusborne <tom@edge22.com>
2017-05-31 00:59:21 -0700

    Make sure controls are only required once

--- inc/customizer/customizer-helpers.php
+++ inc/customizer/customizer-helpers.php
@@ -2,14 +2,14 @@
 defined( 'WPINC' ) or die;
 
 // Controls
-require trailingslashit( dirname( __FILE__ ) ) . 'controls/class-range-control.php';
-require trailingslashit( dirname( __FILE__ ) ) . 'controls/class-typography-control.php';
-require trailingslashit( dirname( __FILE__ ) ) . 'controls/class-upsell-section.php';
-require trailingslashit( dirname( __FILE__ ) ) . 'controls/class-upsell-control.php';
-require trailingslashit( dirname( __FILE__ ) ) . 'controls/class-deprecated.php';
+require_once trailingslashit( dirname( __FILE__ ) ) . 'controls/class-range-control.php';
+require_once trailingslashit( dirname( __FILE__ ) ) . 'controls/class-typography-control.php';
+require_once trailingslashit( dirname( __FILE__ ) ) . 'controls/class-upsell-section.php';
+require_once trailingslashit( dirname( __FILE__ ) ) . 'controls/class-upsell-control.php';
+require_once trailingslashit( dirname( __FILE__ ) ) . 'controls/class-deprecated.php';
 
 // Helper functions
-require trailingslashit( dirname( __FILE__ ) ) . 'helpers.php';
+require_once trailingslashit( dirname( __FILE__ ) ) . 'helpers.php';
 
 // Deprecated
-require trailingslashit( dirname( __FILE__ ) ) . 'deprecated.php';
+require_once trailingslashit( dirname( __FILE__ ) ) . 'deprecated.php';
