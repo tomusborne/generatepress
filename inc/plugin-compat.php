0fa30fb75fa439889219d1158523debf182581a0
tomusborne <tom@edge22.com>
2017-05-31 20:39:21 -0700

    Tell WC to use new attributes function

--- inc/plugin-compat.php
+++ inc/plugin-compat.php
@@ -33,7 +33,7 @@ function generate_woocommerce_start()
 	<div id="primary" <?php generate_content_class();?>>
 		<main id="main" <?php generate_main_class(); ?>>
 			<?php do_action('generate_before_main_content'); ?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_article_schema( 'CreativeWork' ); ?>>
+			<article <?php generate_do_attr( 'post' ); ?>>
 				<div class="inside-article">
 					<?php do_action( 'generate_before_content'); ?>
 					<div class="entry-content" itemprop="text">
