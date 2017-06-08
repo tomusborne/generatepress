dda09311a1a2e117c83ad0897f6c84ea3405dfe4
tomusborne <tom@edge22.com>
2017-06-08 16:45:40 -0700

    Deprecate Generate_Customize_Slider_Control

--- inc/customizer/controls/class-deprecated.php
+++ inc/customizer/controls/class-deprecated.php
@@ -220,3 +220,13 @@ class Generate_Text_Transform_Custom_Control extends WP_Customize_Control
     }
 }
 endif;
+
+if ( ! class_exists( 'Generate_Customize_Slider_Control' ) ) :
+/**
+ * Create our container width slider control
+ * @deprecated 1.3.47
+ */
+class Generate_Customize_Slider_Control extends WP_Customize_Control {
+	public function render_content() {}
+}
+endif;
