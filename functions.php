e19cc91fe7fd47de2f3fe05ee277e3fa26b52ce4
tomusborne <tom@edge22.com>
2017-06-09 19:56:31 -0700

    1.3.48

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 // No direct access, please
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'GENERATE_VERSION', '1.3.47' );
+define( 'GENERATE_VERSION', '1.3.48' );
 define( 'GENERATE_URI', get_template_directory_uri() );
 define( 'GENERATE_DIR', get_template_directory() );
 
