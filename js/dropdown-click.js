1424a77f3ec28cfe51b01ab8221a9aa2fca9a5b2
tomusborne <tom@edge22.com>
2017-09-15 15:44:32 -0700

    Remove JS checks for dropdown hover type

--- js/dropdown-click.js
+++ js/dropdown-click.js
@@ -40,69 +40,66 @@
 		}
 
 		// Do stuff if click dropdown if enabled
-		if ( body.classList.contains( 'dropdown-click' ) ) {
-			var parentElementLinks = document.querySelectorAll( '.main-nav .menu-item-has-children > a' );
+		var parentElementLinks = document.querySelectorAll( '.main-nav .menu-item-has-children > a' );
 
-			// Open the sub-menu by clicking on the entire link element
-			if ( body.classList.contains( 'dropdown-click-menu-item' ) ) {
-				for ( var i = 0; i < parentElementLinks.length; i++ ) {
-					parentElementLinks[i].addEventListener( 'click', dropdownClick, true );
-				}
+		// Open the sub-menu by clicking on the entire link element
+		if ( body.classList.contains( 'dropdown-click-menu-item' ) ) {
+			for ( var i = 0; i < parentElementLinks.length; i++ ) {
+				parentElementLinks[i].addEventListener( 'click', dropdownClick, true );
 			}
-
-			// Open the sub-menu by clicking on a dropdown arrow
-			if ( body.classList.contains( 'dropdown-click-arrow' ) ) {
-				// Add a class to sub-menu items that are set to #
-				for ( var i = 0; i < document.querySelectorAll( '.main-nav .menu-item-has-children > a' ).length; i++ ) {
-					if ( '#' == document.querySelectorAll( '.main-nav .menu-item-has-children > a' )[i].getAttribute( 'href' ) ) {
-						document.querySelectorAll( '.main-nav .menu-item-has-children > a' )[i].classList.add( 'menu-item-dropdown-click' );
-					}
-				}
-
-				var dropdownToggleLinks = document.querySelectorAll( '.main-nav .menu-item-has-children > a .dropdown-menu-toggle' );
-				for ( var i = 0; i < dropdownToggleLinks.length; i++ ) {
-					dropdownToggleLinks[i].addEventListener( 'click', dropdownClick, false );
-
-					dropdownToggleLinks[i].addEventListener( 'keydown', function( e ) {
-						var _this = this;
-						var key = e.which || e.keyCode;
-						if ( key === 13 ) { // 13 is enter
-							dropdownClick( e, _this );
-						}
-					}, false );
-				}
-
-				for ( var i = 0; i < document.querySelectorAll( '.main-nav .menu-item-has-children > a.menu-item-dropdown-click' ).length; i++ ) {
-					document.querySelectorAll( '.main-nav .menu-item-has-children > a.menu-item-dropdown-click' )[i].addEventListener( 'click', dropdownClick, false );
-				}
-			}
-
-			var closeSubMenus = function() {
-				if ( document.querySelector( 'nav ul .toggled-on' ) ) {
-					var activeSubMenus = document.querySelectorAll( 'nav ul .toggled-on' );
-					for ( var i = 0; i < activeSubMenus.length; i++ ) {
-						activeSubMenus[i].classList.remove( 'toggled-on' );
-						activeSubMenus[i].closest( '.sfHover' ).classList.remove( 'sfHover' );
-					}
-				}
-			}
-
-			// Close sub-menus when clicking elsewhere
-			document.addEventListener( 'click', function ( event ) {
-				if ( ! event.target.closest( '.sfHover' ) ) {
-					closeSubMenus();
-				}
-			}, false);
-
-			// Close sub-menus on escape key
-			document.addEventListener( 'keydown', function( e ) {
-				var key = e.which || e.keyCode;
-				if ( key === 27 ) { // 27 is esc
-					closeSubMenus();
-				}
-			}, false );
-
 		}
+
+		// Open the sub-menu by clicking on a dropdown arrow
+		if ( body.classList.contains( 'dropdown-click-arrow' ) ) {
+			// Add a class to sub-menu items that are set to #
+			for ( var i = 0; i < document.querySelectorAll( '.main-nav .menu-item-has-children > a' ).length; i++ ) {
+				if ( '#' == document.querySelectorAll( '.main-nav .menu-item-has-children > a' )[i].getAttribute( 'href' ) ) {
+					document.querySelectorAll( '.main-nav .menu-item-has-children > a' )[i].classList.add( 'menu-item-dropdown-click' );
+				}
+			}
+
+			var dropdownToggleLinks = document.querySelectorAll( '.main-nav .menu-item-has-children > a .dropdown-menu-toggle' );
+			for ( var i = 0; i < dropdownToggleLinks.length; i++ ) {
+				dropdownToggleLinks[i].addEventListener( 'click', dropdownClick, false );
+
+				dropdownToggleLinks[i].addEventListener( 'keydown', function( e ) {
+					var _this = this;
+					var key = e.which || e.keyCode;
+					if ( key === 13 ) { // 13 is enter
+						dropdownClick( e, _this );
+					}
+				}, false );
+			}
+
+			for ( var i = 0; i < document.querySelectorAll( '.main-nav .menu-item-has-children > a.menu-item-dropdown-click' ).length; i++ ) {
+				document.querySelectorAll( '.main-nav .menu-item-has-children > a.menu-item-dropdown-click' )[i].addEventListener( 'click', dropdownClick, false );
+			}
+		}
+
+		var closeSubMenus = function() {
+			if ( document.querySelector( 'nav ul .toggled-on' ) ) {
+				var activeSubMenus = document.querySelectorAll( 'nav ul .toggled-on' );
+				for ( var i = 0; i < activeSubMenus.length; i++ ) {
+					activeSubMenus[i].classList.remove( 'toggled-on' );
+					activeSubMenus[i].closest( '.sfHover' ).classList.remove( 'sfHover' );
+				}
+			}
+		}
+
+		// Close sub-menus when clicking elsewhere
+		document.addEventListener( 'click', function ( event ) {
+			if ( ! event.target.closest( '.sfHover' ) ) {
+				closeSubMenus();
+			}
+		}, false);
+
+		// Close sub-menus on escape key
+		document.addEventListener( 'keydown', function( e ) {
+			var key = e.which || e.keyCode;
+			if ( key === 27 ) { // 27 is esc
+				closeSubMenus();
+			}
+		}, false );
 	}
 
 })();
