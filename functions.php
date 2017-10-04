75dcd6e34fcd2f714e93c8a12450d29994fffef2
tomusborne <tom@edge22.com>
2017-10-04 16:37:45 -0700

    Check for Font Awesome setting

--- functions.php
+++ functions.php
@@ -663,6 +663,23 @@ function generate_show_title() {
 }
 endif;
 
+add_filter( 'generate_fontawesome_essentials', 'generate_set_font_awesome_library' );
+/**
+ * Check to see if we should include the full Font Awesome library or not.
+ *
+ * @since 1.5
+ *
+ * @param bool $essentials
+ * @return bool
+ */
+function generate_set_font_awesome_library( $essentials ) {
+	if ( 'essentials' == generate_get_setting( 'font_awesome' ) ) {
+		return true;
+	}
+
+	return $essentials;
+}
+
 if ( ! function_exists( 'generate_update_logo_setting' ) ) :
 /**
  * Migrate the old logo database entry to the new custom_logo theme mod (WordPress 4.5)
