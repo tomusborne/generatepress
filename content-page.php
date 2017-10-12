c66bb62e47539da05eca2c66164ba8dc264fb2b4
tomusborne <tom@edge22.com>
2017-10-12 16:20:09 -0700

    Code cleanup

--- content-page.php
+++ content-page.php
@@ -4,31 +4,32 @@
  *
  * @package GeneratePress
  */
- 
+
 // No direct access, please
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_article_schema( 'CreativeWork' ); ?>>
 	<div class="inside-article">
-		<?php do_action( 'generate_before_content'); ?>
+		<?php do_action( 'generate_before_content' ); ?>
 		
 		<?php if ( generate_show_title() ) : ?>
 			<header class="entry-header">
 				<?php the_title( '<h1 class="entry-title" itemprop="headline">', '</h1>' ); ?>
 			</header><!-- .entry-header -->
 		<?php endif; ?>
-		
+
 		<?php do_action( 'generate_after_entry_header'); ?>
 		<div class="entry-content" itemprop="text">
-			<?php the_content(); ?>
 			<?php
+			the_content();
+
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'generatepress' ),
 				'after'  => '</div>',
 			) );
 			?>
 		</div><!-- .entry-content -->
-		<?php do_action( 'generate_after_content'); ?>
+		<?php do_action( 'generate_after_content' ); ?>
 	</div><!-- .inside-article -->
 </article><!-- #post-## -->
