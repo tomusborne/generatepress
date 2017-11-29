2e2ec892cad86d953fe47e1efeda409c7129567c
tomusborne <tom@edge22.com>
2017-11-28 23:34:17 -0800

    Fix focus issues in navigation search

--- js/navigation-search.js
+++ js/navigation-search.js
@@ -27,15 +27,16 @@
 				item.querySelector( 'i' ).classList.remove( 'fa-close' );
 				item.querySelector( 'i' ).classList.add( 'fa-search' );
 				item.classList.remove( 'active' );
+				document.activeElement.blur();
 				item.classList.remove( 'sfHover' );
 				form.classList.remove( 'nav-search-active' );
 				item.style.float = '';
 			} else {
 				item.classList.add( 'active' );
 				form.classList.add( 'nav-search-active' );
-				form.querySelector( 'input' ).focus();
+				form.querySelector( '.search-field' ).focus();
 
-				// Set a delay to stop conflict with toggleFocus() in dropdown.js
+				// Set a delay to stop conflict with toggleFocus() in a11y.js
 				setTimeout( function() {
 					item.classList.add( 'sfHover' );
 				}, 50 );
