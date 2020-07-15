df2b37f55228d09a1e55bb3e2d5d1151f6016bee
tomusborne <tom@edge22.com>
2020-07-15 09:01:49 -0700

    Add generate_inside_navigation hook to mobile-menu-control-wrapper

--- inc/structure/navigation.php
+++ inc/structure/navigation.php
@@ -119,12 +119,24 @@ function generate_do_header_mobile_menu_toggle() {
 	}
 	?>
 	<div <?php generate_do_element_classes( 'mobile-navigation-toggle', array( 'main-navigation', 'mobile-menu-control-wrapper' ) ); ?>>
+		<?php
+		/**
+		 * generate_inside_navigation hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_navigation_search - 10
+		 * @hooked generate_mobile_menu_search_icon - 10
+		 */
+		do_action( 'generate_inside_navigation', 'mobile-menu-control-wrapper' );
+
 		/**
 		 * generate_before_mobile_menu_button hook
 		 *
 		 * @since 2.5
 		 */
 		do_action( 'generate_before_mobile_menu_button', 'mobile-menu-control-wrapper' );
+		?>
 		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" data-nav="site-navigation">
 			<?php
 			/**
