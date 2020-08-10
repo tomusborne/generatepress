33a3eaa28361e2821b35fd08d69feab54dcbafa5
tomusborne <tom@edge22.com>
2020-08-09 20:44:51 -0700

    Remove un-used variable

--- inc/plugin-compat.php
+++ inc/plugin-compat.php
@@ -310,8 +310,6 @@ function generate_do_pro_compatibility() {
 	}
 
 	if ( 'font' === generate_get_option( 'icons' ) ) {
-		$url = trailingslashit( get_template_directory_uri() );
-
 		if ( defined( 'GENERATE_MENU_PLUS_VERSION' ) ) {
 			$css->set_selector( '.main-navigation .slideout-toggle a:before,.slide-opened .slideout-overlay .slideout-exit:before' );
 			$css->add_property( 'font-family', 'GeneratePress' );
