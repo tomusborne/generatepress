fdf510eba95143eb8c4e5ca5a51589e798dd8f39
tomusborne <tom@edge22.com>
2020-08-10 20:38:53 -0700

    Add widget search button to flex version

--- searchform.php
+++ searchform.php
@@ -14,5 +14,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<span class="screen-reader-text"><?php echo apply_filters( 'generate_search_label', _x( 'Search for:', 'label', 'generatepress' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></span>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr( apply_filters( 'generate_search_placeholder', _x( 'Search &hellip;', 'placeholder', 'generatepress' ) ) ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php echo esc_attr( apply_filters( 'generate_search_label', _x( 'Search for:', 'label', 'generatepress' ) ) ); ?>">
 	</label>
-	<input type="submit" class="search-submit" value="<?php echo apply_filters( 'generate_search_button', _x( 'Search', 'submit button', 'generatepress' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>">
+	<?php
+	if ( generate_is_using_flexbox() ) {
+		printf(
+			'<button class="search-submit" aria-label="%1$s">%2$s</button>',
+			esc_attr( apply_filters( 'generate_search_button', _x( 'Search', 'submit button', 'generatepress' ) ) ),
+			generate_get_svg_icon( 'search' )
+		);
+	} else {
+		printf(
+			'<input type="submit" class="search-submit" value="%s">',
+			apply_filters( 'generate_search_button', _x( 'Search', 'submit button', 'generatepress' ) ) // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		);
+	}
+	?>
 </form>
