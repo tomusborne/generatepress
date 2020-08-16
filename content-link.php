4626c4139d32fe2f4d058009a687841d5f99caf2
tomusborne <tom@edge22.com>
2020-08-16 13:49:17 -0700

    Re-add content-link.php for backward-compatibility
    For people using link formats with a custom content.php without the new title filter.

---
+++ content-link.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * The template for displaying Link post formats.
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_do_microdata( 'article' ); ?>>
+	<div class="inside-article">
+		<?php
+		/**
+		 * generate_before_content hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_featured_page_header_inside_single - 10
+		 */
+		do_action( 'generate_before_content' );
+
+		if ( generate_show_entry_header() ) :
+			?>
+			<header class="entry-header">
+				<?php
+				/**
+				 * generate_before_entry_title hook.
+				 *
+				 * @since 0.1
+				 */
+				do_action( 'generate_before_entry_title' );
+
+				if ( generate_show_title() ) {
+					$params = generate_get_the_title_parameters();
+
+					the_title( $params['before'], $params['after'] );
+				}
+
+				/**
+				 * generate_after_entry_title hook.
+				 *
+				 * @since 0.1
+				 *
+				 * @hooked generate_post_meta - 10
+				 */
+				do_action( 'generate_after_entry_title' );
+				?>
+			</header>
+			<?php
+		endif;
+
+		/**
+		 * generate_after_entry_header hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_post_image - 10
+		 */
+		do_action( 'generate_after_entry_header' );
+
+		$itemprop = '';
+
+		if ( 'microdata' === generate_get_schema_type() ) {
+			$itemprop = ' itemprop="text"';
+		}
+
+		if ( generate_show_excerpt() ) :
+			?>
+
+			<div class="entry-summary"<?php echo esc_html( $itemprop ); ?>>
+				<?php the_excerpt(); ?>
+			</div>
+
+		<?php else : ?>
+
+			<div class="entry-content"<?php echo esc_html( $itemprop ); ?>>
+				<?php
+				the_content();
+
+				wp_link_pages(
+					array(
+						'before' => '<div class="page-links">' . __( 'Pages:', 'generatepress' ),
+						'after'  => '</div>',
+					)
+				);
+				?>
+			</div>
+
+			<?php
+		endif;
+
+		/**
+		 * generate_after_entry_content hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_footer_meta - 10
+		 */
+		do_action( 'generate_after_entry_content' );
+
+		/**
+		 * generate_after_content hook.
+		 *
+		 * @since 0.1
+		 */
+		do_action( 'generate_after_content' );
+		?>
+	</div>
+</article>
