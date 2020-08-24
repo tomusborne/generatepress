439bd7b233eafeb86dfbc6228f4eccc74882b22c
tomusborne <tom@edge22.com>
2020-08-24 11:17:21 -0700

    Hide navigation outer/inner width and alignment settings if set to float

--- inc/customizer/controls/js/customizer-controls.js
+++ inc/customizer/controls/js/customizer-controls.js
@@ -3,7 +3,7 @@
 
 	// Add callback for when the header_textcolor setting exists.
 	api( 'generate_settings[nav_position_setting]', function( setting ) {
-		var isNavFloated, linkSettingValueToControlActiveState;
+		var isNavFloated, isNavAlignable, setNavDropPointActiveState, setNavAlignmentsActiveState;
 
 		/**
 		 * Determine whether the navigation is floating.
@@ -19,11 +19,24 @@
 		};
 
 		/**
+		 * Determine whether the navigation is align-able.
+		 *
+		 * @returns {boolean} Is floating?
+		 */
+		isNavAlignable = function() {
+			if ( 'nav-float-right' === setting.get() || 'nav-float-left' === setting.get() ) {
+				return false;
+			}
+
+			return true;
+		};
+
+		/**
 		 * Update a control's active state according to the navigation location setting's value.
 		 *
 		 * @param {wp.customize.Control} control
 		 */
-		linkSettingValueToControlActiveState = function( control ) {
+		setNavDropPointActiveState = function( control ) {
 			var setActiveState = function() {
 				control.active.set( isNavFloated() );
 			};
@@ -49,8 +62,41 @@
 			setting.bind( setActiveState );
 		};
 
-		// Call linkSettingValueToControlActiveState on the navigation dropdown point.
-		api.control( 'generate_settings[nav_drop_point]', linkSettingValueToControlActiveState );
+		/**
+		 * Update a control's active state according to the navigation location setting's value.
+		 *
+		 * @param {wp.customize.Control} control
+		 */
+		setNavAlignmentsActiveState = function( control ) {
+			var setActiveState = function() {
+				control.active.set( isNavAlignable() );
+			};
+
+			// FYI: With the following we can eliminate all of our PHP active_callback code.
+			control.active.validate = isNavAlignable;
+
+			// Set initial active state.
+			setActiveState();
+
+			/*
+			 * Update activate state whenever the setting is changed.
+			 * Even when the setting does have a refresh transport where the
+			 * server-side active callback will manage the active state upon
+			 * refresh, having this JS management of the active state will
+			 * ensure that controls will have their visibility toggled
+			 * immediately instead of waiting for the preview to load.
+			 * This is especially important if the setting has a postMessage
+			 * transport where changing the setting wouldn't normally cause
+			 * the preview to refresh and thus the server-side active_callbacks
+			 * would not get invoked.
+			 */
+			setting.bind( setActiveState );
+		};
+
+		api.control( 'generate_settings[nav_drop_point]', setNavDropPointActiveState );
+		api.control( 'generate_settings[nav_layout_setting]', setNavAlignmentsActiveState );
+		api.control( 'generate_settings[nav_inner_width]', setNavAlignmentsActiveState );
+		api.control( 'generate_settings[nav_alignment_setting]', setNavAlignmentsActiveState );
 	} );
 
 	var setOption = function( headerAlignment, navLocation, navAlignment ) {
