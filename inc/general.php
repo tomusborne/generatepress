a70da1fcfbfc0f2dd3f954f7930537354195e228
Tom Usborne <tom@edge22.com>
2021-03-24 13:43:37 -0700

    Move a11y javascript inline to footer

--- inc/general.php
+++ inc/general.php
@@ -71,17 +71,11 @@ if ( ! function_exists( 'generate_scripts' ) ) {
 			wp_script_add_data( 'generate-classlist', 'conditional', 'lte IE 11' );
 		}
 
-		if ( apply_filters( 'generate_combine_js', true ) && $suffix ) {
-			wp_enqueue_script( 'generate-main', $dir_uri . "/assets/js/main{$suffix}.js", array(), GENERATE_VERSION, true );
-			$script_handle = 'generate-main';
-		} else {
-			wp_enqueue_script( 'generate-menu', $dir_uri . "/assets/js/menu{$suffix}.js", array(), GENERATE_VERSION, true );
-			wp_enqueue_script( 'generate-a11y', $dir_uri . "/assets/js/a11y{$suffix}.js", array(), GENERATE_VERSION, true );
-			$script_handle = 'generate-menu';
-		}
+		// Register this for later use if needed.
+		wp_enqueue_script( 'generate-menu', $dir_uri . "/assets/js/menu{$suffix}.js", array(), GENERATE_VERSION, true );
 
 		wp_localize_script(
-			$script_handle,
+			'generate-menu',
 			'generatepressMenu',
 			apply_filters(
 				'generate_localize_js_args',
@@ -433,4 +427,20 @@ function generate_set_microdata_markup( $output, $context ) {
 	}
 
 	return $output;
+}
+
+add_action( 'wp_footer', 'generate_do_a11y_scripts' );
+/**
+ * Enqueue scripts in the footer.
+ *
+ * @since 3.1.0
+ */
+function generate_do_a11y_scripts() {
+	if ( apply_filters( 'generate_print_a11y_script', true ) ) {
+		// Add our small a11y script inline.
+		printf(
+			'<script id="generate-a11y">%s</script>',
+			'!function(){"use strict";if("querySelector"in document&&"addEventListener"in window){var e=document.body;e.addEventListener("mousedown",function(){e.classList.add("using-mouse")}),e.addEventListener("keydown",function(){e.classList.remove("using-mouse")})}}();'
+		);
+	}
 }
