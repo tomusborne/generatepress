ae0ec998a722ce6f3cd88a57f27033560727a39e
Tom Usborne <tom@edge22.com>
2021-05-10 15:06:22 -0700

    Add tablet_only and tablet to media query function

--- inc/theme-functions.php
+++ inc/theme-functions.php
@@ -379,7 +379,7 @@ function generate_do_svg_icon( $icon, $replace = false ) {
  */
 function generate_get_media_query( $name ) {
 	$desktop = apply_filters( 'generate_desktop_media_query', '(min-width:1025px)' );
-	$tablet = apply_filters( 'generate_tablet_media_query', '(min-width: 769px) and (max-width: 1024px)' );
+	$tablet_only = apply_filters( 'generate_tablet_media_query', '(min-width: 769px) and (max-width: 1024px)' );
 	$mobile = apply_filters( 'generate_mobile_media_query', '(max-width:768px)' );
 	$mobile_menu = apply_filters( 'generate_mobile_menu_media_query', $mobile );
 
@@ -387,7 +387,8 @@ function generate_get_media_query( $name ) {
 		'generate_media_queries',
 		array(
 			'desktop' => $desktop,
-			'tablet' => $tablet,
+			'tablet_only' => $tablet_only,
+			'tablet' => '(max-width: 1024px)',
 			'mobile' => $mobile,
 			'mobile-menu' => $mobile_menu,
 		)
