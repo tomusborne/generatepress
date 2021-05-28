f407b6c45331384dda005af31437366c8cf355d1
Tom Usborne <tom@edge22.com>
2021-05-27 21:47:59 -0700

    Update dist

--- assets/dist/customizer.asset.php
+++ assets/dist/customizer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9f8ae18248970fb011c5f01d095cc5ae');
+<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b66918fd648e1f9a04c8031558c305d1');
