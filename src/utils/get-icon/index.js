1af092444395101fcdb8f754a49113deeb225232
Tom Usborne <tom@edge22.com>
2021-06-28 10:24:15 -0700

    Add sectionRedirect to GeneratePress_Customize_Field::add_title

--- src/utils/get-icon/index.js
+++ src/utils/get-icon/index.js
@@ -58,4 +58,8 @@ export default function getIcon( icon ) {
 	if ( 'chevron-up' === icon ) {
 		return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24"><path d="M18 15l-6-6-6 6" /></svg>;
 	}
+
+	if ( 'chevron-right' === icon ) {
+		return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24"><path d="M9 18l6-6-6-6" /></svg>;
+	}
 }
