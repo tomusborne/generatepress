07b00a1df7939281e79adbf489d9f276cec91bf6
Tom Usborne <tom@edge22.com>
2021-07-23 12:19:28 -0700

    Update dist

--- assets/dist/dashboard.asset.php
+++ assets/dist/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '820fc2cd46ab389d9a07f14c783435a3');
+<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '9a6cf249c04ff947c8001853b10c73bc');
