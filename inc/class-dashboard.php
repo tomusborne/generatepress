3ecbf8c706d2c8bb161a50cf52c82ee4434440d5
Tom Usborne <tom@edge22.com>
2021-08-12 12:31:05 -0700

    Remove language path from wp_set_script_translations()

--- inc/class-dashboard.php
+++ inc/class-dashboard.php
@@ -208,7 +208,7 @@ class GeneratePress_Dashboard {
 				true
 			);
 
-			wp_set_script_translations( 'generate-dashboard', 'generatepress', GP_PREMIUM_DIR_PATH . 'langs' );
+			wp_set_script_translations( 'generate-dashboard', 'generatepress' );
 
 			wp_localize_script(
 				'generate-dashboard',
