e6caf2be846d52c55f5b455680d8af166f6debfc
Jean Paiva <jeanpaiva29@gmail.com>
2021-08-30 00:36:51 -0300

    Fix typography manager state propagation

--- assets/dist/customizer.asset.php
+++ assets/dist/customizer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'aa2c92070483f5a23785be99e801fd39');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '94df393a3458f4bddac2f3a4937ea267');
