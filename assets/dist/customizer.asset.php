4f6e47e00d74664b13626dcfba77a87f0f7558db
Jean Paiva <jeanpaiva29@gmail.com>
2021-09-13 14:13:06 -0300

    Do not allow negative values for paragraph margin bottom

--- assets/dist/customizer.asset.php
+++ assets/dist/customizer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '3df2fbe4b28c9d57b7784020e144f980');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '65532494b758ce7c59749b870cd2548b');
