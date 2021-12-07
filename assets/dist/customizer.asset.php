612252a7325e1b151ef40ed27dfaa364686782aa
Tom Usborne <tom@edge22.com>
2021-12-07 14:28:44 -0800

    Fix color picker UI

--- assets/dist/customizer.asset.php
+++ assets/dist/customizer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'cce88a99e1e77f1e8791fa8ad107481c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'da1908e9e6ce0313d16a77f4eee7c763');
