b7e06bf98c1d6c5cd350bb9ee845d3df8f12d57c
Tom Usborne <tom@edge22.com>
2022-05-31 10:43:38 -0700

    Prevent content width error in CPTs

--- assets/dist/block-editor.asset.php
+++ assets/dist/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-data', 'wp-dom-ready', 'wp-plugins', 'wp-polyfill'), 'version' => 'f95c209ded2b86963ea8a33a74454ba6');
+<?php return array('dependencies' => array('wp-data', 'wp-dom-ready', 'wp-plugins', 'wp-polyfill'), 'version' => '9c4bddf252b9d2f903d6039565b82224');
