4422945ee43b12d89e95a5fe7bf7bf9ffa29bbbc
Tom Usborne <tom@edge22.com>
2023-03-29 09:28:09 -0600

    Update version

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Set our theme version.
-define( 'GENERATE_VERSION', '3.3.0' );
+define( 'GENERATE_VERSION', '3.3.1' );
 
 if ( ! function_exists( 'generate_setup' ) ) {
 	add_action( 'after_setup_theme', 'generate_setup' );
