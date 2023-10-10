c1125a207a46695ce67cadef51477187c977a4a3
Brian <brian@brianhaas.li>
2023-10-10 14:10:21 +0200

    fix link in pro-items.js

--- src/dashboard/go-pro/pro-items.js
+++ src/dashboard/go-pro/pro-items.js
@@ -39,7 +39,7 @@ export default {
 		icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10" /><circle cx="12" cy="12" r="4" /><path d="M4.93 4.93l4.24 4.24M14.83 14.83l4.24 4.24M14.83 9.17l4.24-4.24M14.83 9.17l3.53-3.53M4.93 19.07l4.24-4.24" /></svg>,
 		action: {
 			label: __( 'Explore our support forums', 'generatepress' ),
-			url: 'https://generatepress.com/support',
+			url: 'https://generate.support/',
 			external: true,
 		},
 	},
